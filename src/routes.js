6bacc0e278bfab3b929fe6e1e95859940927f1f6
Daniel Carvalho <danielbpc2@hotmail.com>
2019-09-19 16:46:18 -0300

    Feat: Basic User Crud
    lacking 2 routes created

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,17 @@
 /* eslint-disable func-names */
 import express from 'express';
+import { toUnicode } from 'punycode';
 import models from './app/models';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.json({ hello: 'working' }));
 // User Routes
-// INDEX
+// Returns All users
 routes.get('/users', (req, res) =>
   models.User.findAll({}).then(user => res.json(user))
 );
-// Create User
+// Create a User
 routes.post('/users', (req, res) => {
   console.log(req.body);
   models.User.create({
@@ -20,6 +21,35 @@ routes.post('/users', (req, res) => {
     active: true,
   }).then(user => {
     res.json(user);
+  });
+});
+
+// Find and return a User
+routes.get('/user/:id', (req, res) => {
+  models.User.findByPk(req.params.id).then(user => res.json(user));
+});
+
+// Update a User
+routes.put('/user/:id', (req, res) => {
+  models.User.findByPk(req.params.id).then(user => {
+    if (user) {
+      user
+        .update({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          active: req.body.active,
+        })
+        .then(user => res.json(user));
+    }
+  });
+});
+
+// Delete a User
+routes.delete('/user/:id', (req, res) => {
+  models.User.findByPk(req.params.id).then(user => {
+    user.destroy();
+    return res.json(user);
   });
 });
 export default routes;
