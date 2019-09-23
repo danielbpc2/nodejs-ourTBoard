27907e38ddd806df73c538c349881e319d09b4c7
Cassio C <lccassiol@gmail.com>
2019-09-23 00:09:11 -0300

    Add password field virtual

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,9 +2,9 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
-      username: DataTypes.STRING,
       email: DataTypes.STRING,
-      password: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+      password_hash: DataTypes.STRING,
       active: DataTypes.BOOLEAN,
     },
     {}
