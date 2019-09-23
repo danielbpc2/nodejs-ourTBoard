c9f4e891c34daf1fc5ee42275ead944918736472
Cassio C <lccassiol@gmail.com>
2019-09-23 00:09:35 -0300

    Organize User Routes

--- src/routes.js
+++ src/routes.js
@@ -1,54 +1,18 @@
-/* eslint-disable func-names */
 import express from 'express';
-import { toUnicode } from 'punycode';
-import models from './app/models';
+
+import UserController from './app/controllers/UserController';
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.json({ hello: 'working' }));
+
 // User Routes
-// Returns All users
-routes.get('/users', (req, res) =>
-  models.User.findAll({}).then(user => res.json(user))
-);
-// Create a User
-routes.post('/users', (req, res) => {
-  models.User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-    active: true,
-  }).then(user => {
-    res.json(user);
-  });
-});
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
 
-// Find and return a User
-routes.get('/user/:id', (req, res) => {
-  models.User.findByPk(req.params.id).then(user => res.json(user));
-});
+routes.post('/users', UserController.store);
 
-// Update a User
-routes.put('/user/:id', (req, res) => {
-  models.User.findByPk(req.params.id).then(user => {
-    if (user) {
-      user
-        .update({
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          active: req.body.active,
-        })
-        .then(user => res.json(user));
-    }
-  });
-});
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
-// Delete a User
-routes.delete('/user/:id', (req, res) => {
-  models.User.findByPk(req.params.id).then(user => {
-    user.destroy();
-    return res.json(user);
-  });
-});
 export default routes;
