ed4d2306349cdc91c8a9f38d5fcc1dea94c7a539
Cassio C <lccassiol@gmail.com>
2019-09-24 16:52:17 -0300

    Addhook before save user

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
@@ -7,11 +9,28 @@ module.exports = (sequelize, DataTypes) => {
       password_hash: DataTypes.STRING,
       active: DataTypes.BOOLEAN,
     },
-    {}
+    {
+      hooks: {
+        beforeSave: async (user, options) => {
+          if (user.password) {
+            user.password_hash = await bcrypt.hash(user.password, 8);
+          }
+        },
+      },
+    }
   );
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Board);
   };
+
+  /* User.methods.generateHash = function(password) {
+    return bcrypt.hash(password, 8);
+  };
+
+  User.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+  }; */
+
   return User;
 };
