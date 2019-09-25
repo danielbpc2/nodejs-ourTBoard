931c95a45385cf1c98648d3608b59875713cf0b8
Cassio C <lccassiol@gmail.com>
2019-09-24 21:23:45 -0300

    Add name to model user duh

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      name: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.VIRTUAL,
       password_hash: DataTypes.STRING,
@@ -19,18 +20,15 @@ module.exports = (sequelize, DataTypes) => {
       },
     }
   );
+
   User.associate = function(models) {
     // associations can be defined here
     User.hasMany(models.Board);
   };
 
-  /* User.methods.generateHash = function(password) {
-    return bcrypt.hash(password, 8);
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password_hash);
   };
-
-  User.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-  }; */
 
   return User;
 };
